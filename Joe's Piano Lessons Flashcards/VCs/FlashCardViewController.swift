465edf30f051cb68ee5daafafea5c4bd0af4c022
Whitney Naquin <joespianolessons@yahoo.com>
2021-02-06 21:42:50 -0600

    cleaned up the code a bit, removed unused properties, added more comments, reindented to prepare to push to main

--- Joe's Piano Lessons Flashcards/VCs/FlashCardViewController.swift
+++ Joe's Piano Lessons Flashcards/VCs/FlashCardViewController.swift
@@ -23,20 +23,18 @@ class FlashCardViewController: UIViewController {
     @IBOutlet weak var grandStaffUIImage: UIImageView!
     @IBOutlet weak var cardsRemaining: UILabel!
     
-    var selectedButtonArrayOfAnswers: [Int]? = nil
     let flashCardVCInstanceOfNicknamesArray = NicknamesArray()
     var finalArrayOfIndexes = [Int]()
     var chosenCountDetailView = [Letter]()
     var wrongAnswerCounter = 0
     var finished = false
     var passingArrayOfLettersSwitchedOff = [Int]()
-    var selectedAnswer = ""
     
     //MARK: Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // create object with flashcard data
+        // every time screen loads create object with flashcard data
         startUpFlashCards()
         model()
     }
@@ -47,81 +45,98 @@ class FlashCardViewController: UIViewController {
         if finished == false {
             //get the last letter in the name of the most recently displayed flashcard and see if it matches the chosen answer
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "A" {
-            model()
-        } else {
-            wrongAnswer(button: AButton)
-        }
+            if lastLetter == "A" {
+                //if they were correct, get them a new card
+                model()
+            } else {
+                //otherwise run the wrong answer method
+                wrongAnswer(button: AButton)
+            }
         }
     }
     @IBAction func BButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "B" {
-            model()
-        } else {
-            wrongAnswer(button: BButton)
-        }
+            if lastLetter == "B" {
+                model()
+            } else {
+                wrongAnswer(button: BButton)
+            }
         }
     }
     @IBAction func CButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "C" {
-            model()
-        } else {
-            wrongAnswer(button: CButton)
-        }
+            if lastLetter == "C" {
+                model()
+            } else {
+                wrongAnswer(button: CButton)
+            }
         }
     }
     @IBAction func DButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "D" {
-            model()
-        } else {
-            wrongAnswer(button: DButton)
+            if lastLetter == "D" {
+                model()
+            } else {
+                wrongAnswer(button: DButton)
+            }
         }
-        }
-        }
+    }
     
     @IBAction func EButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "E" {
-            model()
-        } else {
-            wrongAnswer(button: EButton)
-        }
+            if lastLetter == "E" {
+                model()
+            } else {
+                wrongAnswer(button: EButton)
+            }
         }
     }
     @IBAction func FButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "F" {
-            model()
-        } else {
-            wrongAnswer(button: FButton)
-        }
+            if lastLetter == "F" {
+                model()
+            } else {
+                wrongAnswer(button: FButton)
+            }
         }
     }
     @IBAction func GButtonPressed(_ sender: Any) {
         if finished == false {
             let lastLetter = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname.last!
-        if lastLetter == "G" {
-            model()
-        } else {
-            wrongAnswer(button: GButton)
-        }
+            if lastLetter == "G" {
+                model()
+            } else {
+                wrongAnswer(button: GButton)
+            }
         }
     } //end GButton
+    
+    //MARK: @IBActions
     @IBAction func cartoonButtonPressed(_ sender: Any) {
+        //check to see if the user has finished all cards.  If finished, don't do anything
         if finished == false {
-        performSegue(withIdentifier: Constants.LETTER_SEGUE_FLASHVC_TO_CARTOONVC_IDENTIFIER, sender: Any?.self)
+            //if not finished, go ahead and segue to the cartoon View Controller using the prepare for segue below
+            performSegue(withIdentifier: Constants.LETTER_SEGUE_FLASHVC_TO_CARTOONVC_IDENTIFIER, sender: Any?.self)
         } else {return}
     }
     
-    
+    //MARK: prepare for segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //clean the screen up
+        screenClean()
+        //change the cartoonVC image to the missed card's image
+        let destinationVC = segue.destination as! CartoonViewController
+        destinationVC.passingVar = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname
+        //add the card that was not correctly answered back into the deck so it comes back up later
+        finalArrayOfIndexes.removeLast()
+        //run the model so that apon return to flashcard vc there is a new card showing and one index at 0 in the finalArrayOfIndexes
+        model()
+    }
     //MARK: methods
     
     func startUpFlashCards() {
@@ -129,13 +144,13 @@ class FlashCardViewController: UIViewController {
         
         //code to sift out the unwanted flashcards for the flashcardVC
         if passingArrayOfLettersSwitchedOff.count > 0 {
-        for index in 0...passingArrayOfLettersSwitchedOff.count - 1{
-            chosenCountDetailView.remove(at: passingArrayOfLettersSwitchedOff[index])
+            for index in 0...passingArrayOfLettersSwitchedOff.count - 1{
+                chosenCountDetailView.remove(at: passingArrayOfLettersSwitchedOff[index])
             } // end loop
         }
-            print(chosenCountDetailView)
+        print(chosenCountDetailView)
     } //end start up flashcards func
-
+    
     //MARK:  Model of flashcard math
     func model() {
         //clean up previous card
@@ -152,18 +167,17 @@ class FlashCardViewController: UIViewController {
             }
             //add this card to the list of used cards
             finalArrayOfIndexes.append(randomNumber)
-            //set the flashcard image
+            //MARK: set the flashcard image
             grandStaffUIImage.image = UIImage(named: chosenCountDetailView[randomNumber].letterNickname)
             
-            
+            //update or set the cardsRemaining text
             cardsRemaining.text = "\(chosenCountDetailView.count - finalArrayOfIndexes.count) / \(chosenCountDetailView.count)"
             
             
-
+            
             
         } else {
-            //clean up properties
-            selectedButtonArrayOfAnswers = nil
+            // if all the cards have been displayed, clean up properties and lock all the buttons by marking finished as true
             finalArrayOfIndexes = []
             grandStaffUIImage.image = UIImage(named: "Done")
             finished = true
@@ -172,14 +186,14 @@ class FlashCardViewController: UIViewController {
     }
     
     func wrongAnswer(button: UIButton) {
-        
+        //track wrong answers with red backgrounds and a wrong answer counter.  After two wrong answers diplay the correct cartoon and shuffle that card back into the deck
         wrongAnswerCounter += 1
         button.backgroundColor = .red
         if wrongAnswerCounter == 2 {
             
             //MARK: TODO -  add the proper image
             
-                
+            
             
             performSegue(withIdentifier: Constants.LETTER_SEGUE_FLASHVC_TO_CARTOONVC_IDENTIFIER, sender: Any?.self)
         }
@@ -199,26 +213,7 @@ class FlashCardViewController: UIViewController {
         //reset the wrong answer counter to 0
         wrongAnswerCounter = 0
     }
-    //MARK: prepare for segue
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //clean the screen up
-        screenClean()
-       //change the cartoonVC image to the missed card's image
-        let destinationVC = segue.destination as! CartoonViewController
-        destinationVC.passingVar = chosenCountDetailView[finalArrayOfIndexes.last!].letterNickname
-        //add the card that was not correctly answered back into the deck so it comes back up later
-        finalArrayOfIndexes.removeLast()
-        //run the model so that apon return to flashcard vc there is a new card showing and one index at 0 in the finalArrayOfIndexes
-        model()
-
-    }
+    
 }
-//MARK: to do
-//fix constraints on storyboard
-//add data
-//set the flashcards to display proper quarternote/whole note etc images
-//transfer model to another file???
-//organize the tableview by section of treble, bass, ledger?
-//add 45 sec, 16 sec, full deck, custom buttons
-//save custom settings to UserDefaults
+
 
--- Joe's Piano Lessons Flashcards/VCs/NicknamesViewController.swift
+++ Joe's Piano Lessons Flashcards/VCs/NicknamesViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class NicknamesViewController: UIViewController  {
-
+    
     //MARK: init
     
     @IBOutlet weak var nicknamesTableView: UITableView!
     @IBOutlet weak var nicknamesImage: UIImageView!
-
+    
     var letterArray: [Letter] = []
     var ArrayOfLettersSwitchedOff = [Int]()
     
@@ -21,7 +21,7 @@ class NicknamesViewController: UIViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //assign NicknamesViewController as the delegate and data source for the table view in the NicknamesViewController
         nicknamesTableView.delegate = self
         nicknamesTableView.dataSource = self
@@ -29,24 +29,33 @@ class NicknamesViewController: UIViewController  {
     } //end viewDidLoasd
     
     @objc func switchDidChange(_ sender: UISwitch){
+        //track which switch is being flipped
         print("sender is \(sender.tag)")
         
-    if sender.isOn == true && ArrayOfLettersSwitchedOff.contains(sender.tag) == true {
-        if let i = ArrayOfLettersSwitchedOff.firstIndex(of: sender.tag) {
-            ArrayOfLettersSwitchedOff.remove(at: i)
-        }
-    }// end if
+        //check if the flipped switch is switched on now.  if so check if that card is currently in the "dont use these cards" list
+        if sender.isOn == true && ArrayOfLettersSwitchedOff.contains(sender.tag) == true {
+            //find the index of the card in the "array of letters switched off"
+            if let i = ArrayOfLettersSwitchedOff.firstIndex(of: sender.tag) {
+                //remove that card from the array of letters switched off
+                ArrayOfLettersSwitchedOff.remove(at: i)
+            }
+        }// end if
+        //check if the flipped switch is off now.  if so make sure that card wasn't already removed from the "don't use these cards array"
         if sender.isOn == false && ArrayOfLettersSwitchedOff.contains(sender.tag) == false {
+            //add the selected card to the "don't use these cards array"
             ArrayOfLettersSwitchedOff.append(sender.tag)
         } // end if
     } // end objc
-
+    
     //MARK: prepare for segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //for when the GO! button is pressed to start the flashcard run
+        //sort the array of letters that are not desired from largest to smallest to prepare to remove these letters from the array of All the cards (if this is not done, when someone removes a lower number such as 0, all the other card indexes shift down one index so that the next card to get removed will be attached to the wrong index.  The wrong card will be removed, or worse, the app crashes
         ArrayOfLettersSwitchedOff.sort(by: >)
-                let destinationVC = segue.destination as! FlashCardViewController
+        let destinationVC = segue.destination as! FlashCardViewController
+        //set the arrays of unwanted cards equal in both view controllers
         destinationVC.passingArrayOfLettersSwitchedOff = ArrayOfLettersSwitchedOff
-}
+    }
     
 }// end class
 
@@ -54,36 +63,44 @@ class NicknamesViewController: UIViewController  {
 
 extension NicknamesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //set initialized letterArray var to the data for the flashcards to use in other methods
+        //create an instance of the class that runs the method to create the list of all the cards
         let NicknamesVCInstanceOfNicknamesArray = NicknamesArray()
+        //create the array of all cards
         let letterArray = NicknamesVCInstanceOfNicknamesArray.createArray()
-        //set the class property letterArray so that it can be used in other methods
+        //set the class property letterArray as a list of all the cards as well so that it can be used in other methods
         self.letterArray = letterArray
         
         //tell tableview how many cells it will need based on the flashcard data
         return letterArray.count
-        }
-        
-        
-
+    }
+    
+    
+    
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //pull the data from array for each cell
+        //pull the data from the "array of all cards" for each cell
         let letter = letterArray[indexPath.row]
         
         //assign pulled data to the appropriate cell and its parts
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.NICKNAMES_CELL_REUSE_IDENTIFIER) as! CustomTableViewCell
         
+        //customize the cellview for rounded corners
         cell.cellView.layer.cornerRadius = 20
+        
+        //set the text in each cell to the nicknames of each card
         cell.nicknamesLabel.text = letter.letterNickname
         
         //add switch programmatically so that i can track tag numbers
         let switchView = UISwitch(frame: .zero)
+        //start all switches as ON
         switchView.setOn(true, animated: true)
+        //individualize each switch with a tag
         switchView.tag = indexPath.row
+        //set target for when the switch is activated
         switchView.addTarget(self, action: #selector(self.switchDidChange(_:)), for: .valueChanged)
+        //add the switch to the accessory view of each cell
         cell.accessoryView = switchView
         
         //apply to tableview
@@ -95,9 +112,9 @@ extension NicknamesViewController: UITableViewDelegate, UITableViewDataSource {
         //when a cell is selected display the cartoon for that cell
         nicknamesImage.image = UIImage(named: letterArray[indexPath.row].letterNickname)
         
+        
+        
+        
+    }
     
-    
-    
-}
-
 }
